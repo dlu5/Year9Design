f2dc4d7bad2707d41f05f2fcf3ab3aa265eab890
Daniel Lu <daniel.lu@Daniel-Lu.local>
2018-10-30 14:18:42 -0400

    diagnostic test answer

---
+++ diagtest.py
@@ -0,0 +1,10 @@
+num = input("enter a num: ")
+num = int(num)
+
+mylist = [1,2,3,4,5]
+for n in my list:
+	if(n == num)
+		print("you found it" + str(num))
+		break
+	else:
+		print(str(num) + "is not equal to" + str(n))
