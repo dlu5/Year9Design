6c73aca45a41a7a7d0dd07afcd0f9208be8a36dc
Daniel Lu <daniel.lu@Y22-Design-Daniel-Lu.local>
2018-11-15 09:06:20 -0500

    updated diagtest.py

--- diagtest.py
+++ diagtest.py
@@ -1,10 +1,16 @@
+# get user input using inpu() function
 num = input("enter a num: ")
 num = int(num)
+# converting/casting string to a num
+# could also do: num = int(input("enter a num: ")) but may be less readable
 
 mylist = [1,2,3,4,5]
-for n in my list:
-	if(n == num)
-		print("you found it" + str(num))
-		break
-	else:
-		print(str(num) + "is not equal to" + str(n))
+
+# don't need to define "n" because the variable is auto assigned every integer in the list 
+for n in mylist:
+    if (n == num):
+        # string concatenation; not strings and ints and floats
+        print(" you found it " + str(num))
+        break
+    else:
+        print(str(num) + " is not equal to " + str(n))
