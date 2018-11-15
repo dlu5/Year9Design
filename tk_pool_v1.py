b68925fb6c0ca8928edbb1476adbc03d104e9b98
Daniel Lu <daniel.lu@Y22-Design-Daniel-Lu.local>
2018-11-15 12:13:54 -0500

    added save to dialog and web scraping

--- tk_pool_v1.py
+++ tk_pool_v1.py
@@ -1,5 +1,18 @@
 from tkinter import *
+from tkinter import messagebox
+from tkinter import filedialog
+import requests
+from bs4 import BeautifulSoup
 
+def saveList():
+    myfile = open("myplayers.txt", "w")
+    for player in lst:
+        myfile.write(player + "\n")
+    myfile.close()
+    root.filename = filedialog.asksaveasfilename(initialdir = "/", title = "Select file")
+    print (root.filename)
+    messagebox.showinfo("myplayers.txt", "Players saved to disk")
+    
 def updatelab():
     lstprint = ""
     for item in lst:
@@ -20,9 +33,28 @@ def removeItem():
         entry.delete(0, END)
         updatelab()
         
+def scrape():
+    if (messagebox.askyesno("Wait?", "This could take a few seconds. Wait?") == False):
+        return
+    
+    if site.status_code is 200:
+        content = BeautifulSoup(site.content, "html.parser")
+        totalpts = 0
+        for myplayer in lst: #loop to check my players
+            dTag = content.find(attrs={"csk": myplayer})
+            parent = dTag.findParent("tr")
+            playerpts = int(parent.contents[8].text) # 8th tag is total points
+            print(myplayer + " " + str(playerpts))
+            totalpts = totalpts + playerpts
+        mypts.configure(text=totalpts)
+    
+        
 # these are our variables       
 lst = []
 lstprint = ""
+totalpts = 0
+print("Downloading hockey data")
+site = requests.get("https://www.hockey-reference.com/leagues/NHL_2019_skaters.html")
 
 root = Tk()
 
@@ -41,15 +73,29 @@ entry.pack()
 
 # adds the "Add" button
 addbutton = Button(root, text="Add", fg="blue", command=addItem)
-
 addbutton.pack()
 
-removebutton = Button(root, text="Remove", fg="blue", command=removeItem)
+# adds remove button
+removebutton = Button(root, text="Remove", fg="red", command=removeItem)
 removebutton.pack()
+
+savebutton = Button(root, text="Save", fg="springgreen2", command=saveList)
+savebutton.pack()
 
 # where to print input
 mylab = Label(root, text=lstprint,anchor=W, justify=LEFT)
 mylab.pack()
+
+# Label for checking pts
+mylab = Label(root, text=lstprint,anchor=W,justify=LEFT)
+mylab.pack()
+
+ptsbutton = Button(root, text = "Check pts", command=scrape)
+ptsbutton.pack()
+
+mypts = Label(root,text=totalpts)
+mypts.pack()
+
 
 
 
